9d7a99c7f7916fcfb468af694dcab5f93ff98cbd
e <6410762+e-mit@users.noreply.github.com>
2024-03-10 22:07:42 +0000

    Get VPCsubnetIDlist and VPCsecurityGroupID via CLI calls

--- setup.sh
+++ setup.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
+# Name prefix for the resources to be created:
 NAME_PREFIX=test1
 
-source create.sh clean
+# Security group in the DB's VPC to use:
+SEC_GRP_NAME="lambda-rds-1"
+RDS_INSTANCE_NAME=testdbi
 
-source create.sh stack \
-"VPCsecurityGroupID=sg-02d35092e3cdbbf42 \
-VPCsubnetIDlist=subnet-0e296e482bfaeebb5,subnet-0880df993af0d3b64,subnet-0f52841fea838974e"
+####################################################
 
 source config.sh
 
+source create.sh clean
+
+# Get the Security Group ID corresponding to SEC_GRP_NAME
+SEC_GRP_ID=$(aws ec2 describe-security-groups \
+--group-names $SEC_GRP_NAME | \
+python3 -c \
+"import sys, json
+print(json.load(sys.stdin)['SecurityGroups'][0]['GroupId'])")
+
+# List the subnets which the RDS is on
+SUBNETS=$(aws rds describe-db-instances \
+--db-instance-identifier $RDS_INSTANCE_NAME | \
+python3 -c \
+"import sys, json
+sng = json.load(sys.stdin)['DBInstances'][0]['DBSubnetGroup']
+subnets = sng['Subnets'][0]['SubnetIdentifier']
+for s in sng['Subnets'][1:]:
+    subnets += (',' + s['SubnetIdentifier'])
+print(subnets)")
+
+
+source create.sh stack \
+"VPCsecurityGroupID=$SEC_GRP_ID VPCsubnetIDlist=$SUBNETS"
+
+# Add environment variables to the lambda
 aws lambda update-function-configuration \
 --function-name $FUNCTION_NAME \
 --environment "Variables={DB_PORT=$DB_PORT, \
