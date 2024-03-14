80f625cdf60644b01caa86ff8f64062ad35ffb7c
e <6410762+e-mit@users.noreply.github.com>
2024-03-14 18:29:58 +0000

    Extract an abstract base class and have separate sqlite and psql classes

--- tests/sqlite_helper.py
+++ tests/sqlite_helper.py
@@ -2,20 +2,31 @@ from pathlib import Path
 import tempfile
 import sqlite3
 from typing import Any
+from abc import ABC, abstractmethod
 
-from sqlmodel import SQLModel, create_engine
+import psycopg2
+from psycopg2 import sql
 
 
-class SQLiteHelper:
+class DBhelper(ABC):
 
-    def __init__(self) -> None:
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self.temp_db_file = Path(self.temp_dir.name) / "temp.db"
-        self.engine = create_engine(f"sqlite:///{self.temp_db_file}")
-        SQLModel.metadata.create_all(self.engine)
-        self.conn = sqlite3.connect(self.temp_db_file)
+    @abstractmethod
+    def connect(self, **kwargs) -> Any:
+        ...
+
+    @property
+    @abstractmethod
+    def list_table_query(self) -> str:
+        ...
+
+    def __init__(self, dbname: str = '', **kwargs):
+        """Connect to the database."""
+        self.dbname = dbname
+        kwargs['dbname'] = dbname
+        self.conn = self.connect(**kwargs)
 
     def execute(self, query: str) -> list[Any]:
+        """Execute a query and return data as a list."""
         cursor = self.conn.cursor()
         cursor.execute(query)
         values = cursor.fetchall()
@@ -27,10 +38,59 @@ class SQLiteHelper:
         return data[0][0]
 
     def get_table_names(self) -> list[str]:
-        tables = self.execute(
-            "SELECT name FROM sqlite_master WHERE type='table';")
+        tables = self.execute(self.list_table_query)
         return [x[0] for x in tables]
 
     def tearDown(self) -> None:
         self.conn.close()
+
+
+class SQLiteHelper(DBhelper):
+
+    def connect(self, **kwargs) -> sqlite3.Connection:
+        return sqlite3.connect(self.dbname)
+
+    @property
+    def list_table_query(self) -> str:
+        return "SELECT name FROM sqlite_master WHERE type='table';"
+
+    def __init__(self, dbname: str = '', **kwargs):
+        """Create an empty database."""
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.dbname = str(Path(self.temp_dir.name) / "temp.db")
+        super().__init__(dbname=self.dbname)
+
+    def tearDown(self) -> None:
+        """Delete the database."""
+        super().tearDown()
         self.temp_dir.cleanup()
+
+
+class PSQLHelper(DBhelper):
+
+    def connect(self, **kwargs):
+        return psycopg2.connect(**kwargs)
+
+    @property
+    def list_table_query(self) -> str:
+        return ("SELECT table_name FROM information_schema.tables "
+                "where table_schema='public';")
+
+    def drop_tables(self):
+        table_names = self.get_table_names()
+        for table_name in table_names:
+            cur = self.conn.cursor()
+            cur.execute(sql.SQL('DROP TABLE {table};').format(
+                table=sql.Identifier(table_name)))
+            self.conn.commit()
+            cur.close()
+
+    def __init__(self, dbname: str = '', **kwargs):
+        """Connect to the database and delete all tables."""
+        super().__init__(dbname, **kwargs)
+        self.drop_tables()
+
+    def tearDown(self) -> None:
+        """Delete all tables but leave the database."""
+        self.drop_tables()
+        super().tearDown()
