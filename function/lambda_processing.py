c1cc1c35f817ce0a0c4aea1921a8d31feafe382f
e <6410762+e-mit@users.noreply.github.com>
2024-03-14 18:27:33 +0000

    Add create_sql_engine() function

--- function/lambda_processing.py
+++ function/lambda_processing.py
@@ -1,13 +1,42 @@
 from typing import Any
 import logging
 
-from sqlalchemy import Engine
+from sqlalchemy import Engine, URL
+from sqlmodel import create_engine
 
 import model
 import sqs_event
 from function import sql_model
 
 logger = logging.getLogger()
+
+
+def create_sql_engine(db_name: str, db_user: str | None, db_host: str | None,
+                      db_port: str | int | None = None,
+                      db_password: str | None = None,
+                      db_dialect_driver: str = 'postgresql+psycopg2'
+                      ) -> Engine:
+    try:
+        db_port_int = int(db_port)  # type: ignore
+    except Exception:
+        db_port_int = None
+
+    if db_password == '':
+        db_password = None
+    if db_user == '':
+        db_user = None
+    if db_host == '':
+        db_host = None
+
+    url_object = URL.create(
+        db_dialect_driver,
+        username=db_user,
+        password=db_password,
+        host=db_host,
+        database=db_name,
+        port=db_port_int
+    )
+    return create_engine(url_object)
 
 
 def lambda_processing(event: dict[str, Any], engine: Engine):
@@ -19,6 +48,6 @@ def lambda_processing(event: dict[str, Any], engine: Engine):
                 engine, source_api_data.data[0])
 
     except Exception as e:
-        logger.info("Event data extraction failed.")
         logger.error(e)
+        logger.info("Event data extraction failed.")
         raise e
