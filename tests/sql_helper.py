9eb3520e6938770729f03190445e89feabe0f3b2
e <6410762+e-mit@users.noreply.github.com>
2024-03-15 10:38:05 +0000

    Improve database parameter setting method

--- tests/sql_helper.py
+++ tests/sql_helper.py
@@ -13,9 +13,15 @@ from psycopg2 import sql
 
 class DBhelper(ABC):
     """Do simple CRUD operations on an undefined SQL database."""
+    dbname: str
+    user: str | None
+    host: str | None
+    port: str | None
+    password: str | None
+    dialect_driver: str
 
     @abstractmethod
-    def connect(self, **kwargs) -> Any:
+    def connect(self) -> Any:
         ...
 
     @property
@@ -23,11 +29,13 @@ class DBhelper(ABC):
     def list_table_query(self) -> str:
         ...
 
-    def __init__(self, dbname: str = '', **kwargs):
+    @abstractmethod
+    def set_environment_variables(self) -> None:
+        ...
+
+    def __init__(self):
         """Connect to the database."""
-        self.dbname = dbname
-        kwargs['dbname'] = dbname
-        self.conn = self.connect(**kwargs)
+        self.conn = self.connect()
 
     def execute(self, query: str) -> list[Any]:
         """Execute a query and return data as a list."""
@@ -48,26 +56,23 @@ class DBhelper(ABC):
     def tearDown(self) -> None:
         self.conn.close()
 
-    @abstractmethod
-    def set_environment_variables(self) -> None:
-        ...
-
 
 class SQLiteHelper(DBhelper):
     """Create a SQLite database for CRUD, then delete the whole database."""
 
-    def connect(self, **kwargs) -> sqlite3.Connection:
+    def connect(self) -> sqlite3.Connection:
         return sqlite3.connect(self.dbname)
 
     @property
     def list_table_query(self) -> str:
         return "SELECT name FROM sqlite_master WHERE type='table';"
 
-    def __init__(self, dbname: str = '', **kwargs):
+    def __init__(self):
         """Create an empty database."""
+        self.dialect_driver = 'sqlite'
         self.temp_dir = tempfile.TemporaryDirectory()
         self.dbname = str(Path(self.temp_dir.name) / "temp.db")
-        super().__init__(dbname=self.dbname)
+        super().__init__()
 
     def tearDown(self) -> None:
         """Delete the database."""
@@ -80,14 +85,18 @@ class SQLiteHelper(DBhelper):
         os.environ['DB_PASSWORD'] = ''
         os.environ['DB_HOST'] = ''
         os.environ['DB_PORT'] = ''
-        os.environ['DB_DIALECT_DRIVER'] = 'sqlite'
+        os.environ['DB_DIALECT_DRIVER'] = self.dialect_driver
 
 
 class PSQLHelper(DBhelper):
     """Connect to an existing postgres database, drop all its tables."""
 
-    def connect(self, **kwargs):
-        return psycopg2.connect(**kwargs)
+    POSTGRES_DIALECT = 'postgresql+psycopg2'
+
+    def connect(self):
+        kw = {k: getattr(self, k) for k in
+              ['dbname', 'user', 'host', 'port', 'password']}
+        return psycopg2.connect(**kw)
 
     @property
     def list_table_query(self) -> str:
@@ -103,9 +112,10 @@ class PSQLHelper(DBhelper):
             self.conn.commit()
             cur.close()
 
-    def __init__(self, dbname: str = '', **kwargs):
+    def __init__(self):
         """Connect to the database and delete all tables."""
-        super().__init__(dbname, **kwargs)
+        self.get_test_environment_variables()
+        super().__init__()
         self.drop_tables()
 
     def tearDown(self) -> None:
@@ -113,5 +123,22 @@ class PSQLHelper(DBhelper):
         self.drop_tables()
         super().tearDown()
 
-    def set_environment_variables(self) -> None:
-        ...  # TODO
+    def get_test_environment_variables(self) -> None:
+        """Raises KeyError if var(s) not set."""
+        self.user = os.environ['TEST_DB_USER']
+        self.dbname = os.environ['TEST_DB_NAME']
+        self.host = os.getenv('TEST_DB_HOST', None)
+        self.port = os.getenv('TEST_DB_PORT', None)
+        self.password = os.getenv('TEST_DB_PASSWORD', None)
+        self.dialect_driver = os.getenv('TEST_DB_DIALECT_DRIVER',
+                                        self.POSTGRES_DIALECT)
+        if self.dialect_driver != self.POSTGRES_DIALECT:
+            raise ValueError("Unexpected database dialect/driver.")
+
+    def set_environment_variables(self):
+        os.environ['DB_USER'] = self.user or ''
+        os.environ['DB_NAME'] = self.dbname
+        os.environ['DB_PASSWORD'] = self.password or ''
+        os.environ['DB_HOST'] = self.host or ''
+        os.environ['DB_PORT'] = self.port or ''
+        os.environ['DB_DIALECT_DRIVER'] = self.POSTGRES_DIALECT
