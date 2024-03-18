d8744e070e6554cb8b27c486b044b19a1fff59e8
e <6410762+e-mit@users.noreply.github.com>
2024-03-18 13:21:02 +0000

    Extract DataModelTable as a generic class. NB: id is shadowed.

--- function/sql_model.py
+++ function/sql_model.py
@@ -1,15 +1,13 @@
 """SQL database model for https://api.carbonintensity.org.uk/intensity"""
 
-from typing import Optional, Annotated, Literal, overload
+from typing import Annotated
 from typing_extensions import Self
 from datetime import datetime
 
 from pydantic import AfterValidator
-from sqlmodel import Field, SQLModel, Session
-from sqlmodel.sql.expression import SelectOfScalar
-from sqlalchemy import Engine
 
 import source_model
+import sqlmodel
 import sql_model_base
 
 
@@ -40,68 +38,10 @@ class CarbonIntensityRecord(
                    time=midpoint)
 
 
-class CarbonIntensityTable(CarbonIntensityRecord, table=True):
+class CarbonIntensityTable(
+        CarbonIntensityRecord,
+        sql_model_base.DataModelTable[
+            CarbonIntensityRecord, source_model.CarbonIntensityData],
+        table=True):
     """This is used as the database interface (does not perform validation)."""
-    __tablename__: str = CarbonIntensityRecord.__tablename__  # type: ignore
-    id: Optional[int] = Field(default=None, primary_key=True)
-
-    @classmethod
-    def _create_table_item_from_model(cls,
-                                      entry: CarbonIntensityRecord) -> Self:
-        return cls(**{k: getattr(entry, k) for k in entry.model_fields})
-
-    @classmethod
-    def _create_model_from_table_item(cls,
-                                      item: Self) -> CarbonIntensityRecord:
-        return CarbonIntensityRecord(
-            **{k: getattr(item, k) for k in item.model_fields})
-
-    @classmethod
-    def _add(cls, engine: Engine, item: Self) -> None:
-        with Session(engine) as session:
-            session.add(item)
-            session.commit()
-
-    @classmethod
-    def add_from_db_model(cls, engine: Engine,
-                          entry: CarbonIntensityRecord) -> None:
-        """Use an object that was validated to match the database schema."""
-        item = cls._create_table_item_from_model(entry)
-        cls._add(engine, item)
-
-    @classmethod
-    def add_from_source_model(cls, engine: Engine,
-                              source_data: source_model.CarbonIntensityData
-                              ) -> None:
-        """Use an object that was validated to match the source API."""
-        item = CarbonIntensityRecord.from_source_model(source_data)
-        cls.add_from_db_model(engine, item)
-
-    @overload
-    @classmethod
-    def _read(cls, engine: Engine, method: Literal['all'],
-              statement: SelectOfScalar[Self]) -> list[Self]: ...
-
-    @overload
-    @classmethod
-    def _read(cls, engine: Engine, method: Literal['first'],
-              statement: SelectOfScalar[Self]) -> Self: ...
-
-    @classmethod
-    def _read(cls, engine: Engine, method: Literal['all', 'first'],
-              statement: SelectOfScalar[Self]) -> list[Self] | Self:
-        with Session(engine) as session:
-            return getattr(session.exec(statement), method)()
-
-    @classmethod
-    def read_all(cls, engine: Engine,
-                 statement: SelectOfScalar[Self]
-                 ) -> list[CarbonIntensityRecord]:
-        return [cls._create_model_from_table_item(x) for x in
-                cls._read(engine, 'all', statement)]
-
-    @classmethod
-    def read_first(cls, engine: Engine,
-                   statement: SelectOfScalar[Self]) -> CarbonIntensityRecord:
-        return cls._create_model_from_table_item(
-                        cls._read(engine, 'first', statement))
+    id: int | None = sqlmodel.Field(default=None, primary_key=True)
