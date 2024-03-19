aff5befd68a3324bf7e0bf6068a209f89d81e363
e <6410762+e-mit@users.noreply.github.com>
2024-03-19 13:40:54 +0000

    Add linting tests to test script

--- test.sh
+++ test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -x #echo on
 
 python -m unittest discover tests
+
+
+# MYPYPATH=tests/stubs
+
+python -m pytest tests -s
+python -m bandit -r . --exclude=/tests/,/venv/,/package/
+python -m pytest --cov=. tests
+python -m flake8 --exclude=tests/*,venv/*,package/*
+python -m mypy . --exclude 'tests/' --exclude 'venv/' --exclude 'package/'
+python -m pycodestyle function/*.py
+python -m pydocstyle function/*.py --ignore=D107,D203,D213
+python -m pylint function/*.py
+python -m pyright *.py
